5554fffacab85f4fce6d4ebe0142c07e6ee431da
khadmalek <k.abdelmalek@live.fr>
2024-07-23 23:38:09 +0200

    Create a function named cleanSet that returns a string of all the set values that start with a specific string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,25 @@
+function cleanSet(set, startString) {
+    // Vérifier si startString est une chaîne vide ou non
+    if (startString === '') {
+      return '';
+    }
+  
+    // Initialiser un tableau pour contenir les parties des chaînes
+    const result = [];
+  
+    // Parcourir chaque élément du set
+    set.forEach((value) => {
+      // Vérifier si l'élément commence par startString
+      if (value.startsWith(startString)) {
+        // Ajouter la partie restante de la chaîne au tableau
+        result.push(value.slice(startString.length));
+      }
+    });
+  
+    // Joindre les éléments du tableau avec un séparateur -
+    return result.join('-');
+  }
+  
+  // Exporter la fonction pour qu'elle puisse être importée ailleurs
+  export default cleanSet;
+  
