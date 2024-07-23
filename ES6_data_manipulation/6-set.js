2dcb53a747be2e367b544ef8af95742a7000edf2
khadmalek <k.abdelmalek@live.fr>
2024-07-23 23:31:54 +0200

    Create a function named setFromArray that returns a Set from an array

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,11 @@
+function setFromArray(array) {
+    // Créer un nouveau Set à partir du tableau
+    const newSet = new Set(array);
+  
+    // Retourner le Set
+    return newSet;
+  }
+  
+  // Exporter la fonction pour qu'elle puisse être importée ailleurs
+  export default setFromArray;
+  
