28ceed251355f36039cadb96b05269d558160a1c
khadmalek <k.abdelmalek@live.fr>
2024-07-24 11:56:08 +0200

    fix eslint

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,14 @@
 function getListStudents() {
-    // Étape 3: Créer les objets des étudiants
-    const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  
-    // Étape 4: Retourner le tableau d'objets
-    return students;
-  }
-  
-  // Exporter la fonction pour qu'elle puisse être importée ailleurs
-  export default getListStudents;
-  
+  // Étape 3: Créer les objets des étudiants
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  // Étape 4: Retourner le tableau d'objets
+  return students;
+}
+
+// Exporter la fonction pour qu'elle puisse être importée ailleurs
+export default getListStudents;
